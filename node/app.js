4f60f2aa4ac646da8efb13285256a009459cc2b6
Your Name <you@example.com>
2020-08-26 14:34:58 +0530

    mongodb implemented

--- node/app.js
+++ node/app.js
@@ -1,19 +1,40 @@
 const express=require('express');
+const mongo= require('mongoose');
+const e = require('express');
+
+var schema = mongo.Schema({
+    name: String,
+    age: Number
+  });
+  var Model = mongo.model("model", schema, "myCollection");
+  var doc1 = new Model({ name: "John", age: 21 });
+ 
+
+
 const app=express();
 app.use(express.json());
 const getres='you got the request';
 app.post('/post',async(req,res,next)=>{
 const name= req.body.name;
 console.log(name);
+club.collection.save(data);
 next();
 }
 );
 
 app.get('/get',async(req,res,next)=>{
-res.end(getres);
-res.json('hey from nodejs');
-next();
-}
-);
-
+    doc1.save(function(err, doc) {
+        if (err) return console.error(err);
+        console.log("Document inserted succussfully!");
+      });
+});
 app.listen(3000);
+mongo.connect('mongodb+srv://himu:himu@cluster0.szkh7.mongodb.net/test',{useNewUrlParser:true,useUnifiedTopology:true});
+
+mongo.connection.once('open',()=>console.log('connected'))
+.on('error',(e)=>{
+console.log(error);
+});
+var db=mongo.connection;
+   
+ 
